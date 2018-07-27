549877e792faf1447e3b50c4e3fc21443898b81a
Jayden Sansom <jaydensansom@gmail.com>
2018-07-27 13:21:36 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -66,10 +66,6 @@ def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
   # loop if steps is equal to 6
   
-  loop do 
-    puts "Hello"
-    break
-    puts "Goodbye"
-  end
+
   
 end
